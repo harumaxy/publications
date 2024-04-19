fc1be41af21b63f1bb84bd94f2d47eef7fd0285e
harumaxy <harumaxy@gmail.com>
2024-04-19 21:35:41 +0900

    fix for biome

--- uploadImage.ts
+++ uploadImage.ts
@@ -4,11 +4,11 @@ import * as cb from "https://deno.land/x/copy_paste/mod.ts";
 
 // setup
 const s3 = new AWS.S3({
-  endpoint: Deno.env.get("R2_ENDPOINT")!,
+  endpoint: Deno.env.get("R2_ENDPOINT") ?? "",
   region: "auto",
   credentials: {
-    accessKeyId: Deno.env.get("R2_ACCESS_KEY_ID")!,
-    secretAccessKey: Deno.env.get("R2_SECRET_ACCESS_KEY")!,
+    accessKeyId: Deno.env.get("R2_ACCESS_KEY_ID") ?? "",
+    secretAccessKey: Deno.env.get("R2_SECRET_ACCESS_KEY") ?? "",
   },
 });
 
@@ -17,7 +17,7 @@ const s3 = new AWS.S3({
 const filepath = Deno.args[0] ?? (await cb.readText());
 const filename = filepath.split("/").pop();
 const ext = filename?.split(".").pop();
-if (["png", "jpg", "jpeg", "gif"].indexOf(ext!) === -1) {
+if (["png", "jpg", "jpeg", "gif"].indexOf(ext ?? "") === -1) {
   console.log("This is not image.");
   Deno.exit(1);
 }
@@ -25,13 +25,13 @@ const file = await Deno.readFile(filepath);
 await s3
   .upload({
     Bucket: "blog-images",
-    Key: filename!,
+    Key: filename ?? "",
     ContentType: `image/${ext}`,
     Body: file,
   })
   .promise();
 
 // stdout markdown image link
-const url = encodeURI(`${Deno.env.get("R2_BUCKET_URL")!}/${filename}`);
+const url = encodeURI(`${Deno.env.get("R2_BUCKET_URL") ?? ""}/${filename}`);
 const markdownImg = `![${filename}](${url})`;
 console.log(markdownImg);
